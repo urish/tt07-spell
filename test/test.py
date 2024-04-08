7f86e70302cd53a73f9c07d7f95b0c26408351a9
Uri Shaked <uri@urishaked.com>
2024-04-09 00:36:06 +0300

    test: dummy testbench
    just to test the setup

--- test/test.py
+++ test/test.py
@@ -6,7 +6,7 @@ from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
 @cocotb.test()
-async def test_adder(dut):
+async def test_spell(dut):
   dut._log.info("Start")
   
   # Our example module doesn't use clock and reset, but we show how to use them here anyway.
@@ -24,9 +24,6 @@ async def test_adder(dut):
 
   # Set the input values, wait one clock cycle, and check the output
   dut._log.info("Test")
-  dut.ui_in.value = 20
-  dut.uio_in.value = 30
-
+  
   await ClockCycles(dut.clk, 1)
-
-  assert dut.uo_out.value == 50
+  assert dut.uo_out.value == 1
